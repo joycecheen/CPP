f9d5537bb881b62804bfa93a19e2a345f77f645e
U-BSD\385533 <385533@SHS-10DEC4F83.beaverton.k12.or.us>
2019-09-20 08:28:54 -0700

    update

--- palindrome/palindrome.cpp
+++ palindrome/palindrome.cpp
@@ -3,7 +3,8 @@
 #include <cstring>
 
 /*
- * A program that reads a series of characters and checks to see if it is a palindrome 
+ * A program that reads a series of characters, removes non-letters,
+ * and checks to see if it is a palindrome 
  * Author: Joyce Chen
  * Date: 9/16/2019
  *
@@ -13,28 +14,21 @@ using namespace std;
 
 int main() {
 
-  char input[80];
-  char output[80];
+  char input[80] = {};
+  char output[80] = {};
   int index = 0;
 
   cin.get(input, 80);
   
   for (int i = 0; i < 80; i++) {
-    if (input[i] >= 'a' && input[i] <= 'z') || (input[i] >= 'A' && input[i] <= 'Z') {
+    if ((input[i] >= 'a' && input[i] <= 'z') || (input[i] >= 'A' && input[i] <= 'Z')) {
       output[index] = input[i];
       index++;
     }
   }
 
   output[index] = '\0';
-  
-  /*for (int j = 0; j < 80; j++) {
-    if (!(output[j] >= 'a' && output[j] <= 'z' && output[j] >= 'A' && output[j] <= 'Z')) {
-	output[j] = '\0';
-    }
-  }*/
 
-  
 
   cout << output << endl;
 
