821e6b37709005643f3a5dae1783482d2486a8f4
U-BSD\385533 <385533@SHS-10DECC84C.beaverton.k12.or.us>
2019-09-23 14:06:38 -0700

    palindrome project

--- CPP/palindrome/palindrome.cpp
+++ CPP/palindrome/palindrome.cpp
@@ -5,6 +5,7 @@
 /*
  * A program that reads a series of characters, removes non-letters,
  * and checks to see if it is a palindrome 
+ * 
  * Author: Joyce Chen
  * Date: 9/16/2019
  *
@@ -14,13 +15,17 @@ using namespace std;
 
 int main() {
 
+  // char arrays + variables
   char input[80] = {};
   char output[80] = {};
   int index = 0;
   bool check; 
 
+  // read in users word/phrase
+  cout << "Input: ";
   cin.get(input, 80);
-  
+
+  // take all chars that are letters and put into new array  
   for (int i = 0; i < 80; i++) {
     if ((input[i] >= 'a' && input[i] <= 'z') || (input[i] >= 'A' && input[i] <= 'Z')) {
       output[index] = input[i];
@@ -28,22 +33,26 @@ int main() {
     }
   }
 
+  // add in null character at the end of new array
   output[index] = '\0';
-  cout << output << endl;
 
+  // to check if its a palindrome
+  // compare first to last char, then second to second to last, etc.
   for (int j = 0; j < index; j++) {
+    // set to uppercase
     output[j] = toupper( output[j] );
     output[index - 1] = toupper( output[index - 1] );
     if (output[j] == output[index - 1]) {
       check = true;
       index--;
-    }
+    } 
     else {
       check = false;
       break;
     }
   }
-
+  
+  // print whether or not it is a palindrome based on check
   if (check == true) {
     cout << "Palindrome" << endl;
   }
