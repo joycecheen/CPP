4bf8b86aa9d014727640b42f68ee836bc2e27525
U-BSD\385533 <385533@SHS-10DD3DE98.beaverton.k12.or.us>
2019-10-07 14:31:19 -0700

    yeet

--- CPP/tictactoe/tictactoe.cpp
+++ CPP/tictactoe/tictactoe.cpp
@@ -4,11 +4,10 @@
 
 using namespace std;
 
-void addMove(char move[2], int x, int y);
+//void addMove(char grid [4][4], char move[2], int x, int y);
 void printBoard(char grid[4][4]);
 
-int main() {
-  char move[2] = {};
+char move[2] = {};
   char grid[4][4] =
     {
       {' ', '1', '2', '3'},
@@ -17,28 +16,39 @@ int main() {
       {'c', ' ', ' ', ' '}
     };
   int x;
-  int y;
+  char y;
+
+int main() {
+  
+  //boolean game = 0;
 
   printBoard(grid);
-  addMove(x,y); 
-  /*cin.get(move,3);
-  int x = (move[0] - 48); 
-  int y = (move[1] - 'a' + 1);
-  cout << "x: " << x << endl;
-  cout << "y: " << y << endl;
-  grid[y][x] = 'X';
-  printBoard(grid); */
+  //addMove(x,y);
+  //while (game = 0) {
+    cin.get(move,3);
+    x = (move[0] - 48); 
+    y = (move[1] - 'a' + 1);
+    cout << "x: " << x << endl;
+    cout << "y: " << y << endl;
+      if (grid[y][x] == ' ') {
+        grid[y][x] = 'X';
+
+      }
+      
+    printBoard(grid); 
+    //}
   
   return 0;
 }
 
-void addMove(char move[2], int x, int y) {
+/*void addMove(char grid [4][4], char move[2], int x, int y) {
   cin.get(move,3);
-  int x = (move[0] - 48);
-  int y = (move[1] - 'a' + 1);
-  grid[y][x] = 'X';
+  x = (move[0] - 48);
+  y = move[1];
+  y2 = y - 'a' + 1;
+  grid[y2][x] = 'X';
     return;
-}
+}*/
 
 void printBoard(char grid[4][4]) {
   for (int i = 0; i < 4; ++i) {
