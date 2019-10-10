2ae44ab736e8fbf5d2c03e2e4a2b17023f173bda
U-BSD\385533 <385533@SHS-10DEC4F83.beaverton.k12.or.us>
2019-10-10 09:13:34 -0700

    tic

--- CPP/tictactoe/tictactoe.cpp
+++ CPP/tictactoe/tictactoe.cpp
@@ -4,8 +4,9 @@
 
 using namespace std;
 
-//void addMove(char grid [4][4], char move[2], int x, int y);
-void printBoard(char grid[4][4]);
+void printBoard(int grid[3][3]);
+bool checkWin(int grid[3][3], int player);
+bool checkTie(int grid[3][3]);
 
 const int BLANK = 0;
 const int X_MOVE = 1;
@@ -15,44 +16,127 @@ const int O_TURN = 1;
 
 int main() {
   
-  char move[5];
-  char grid[4][4] =
-  {
-    {' ', '1', '2', '3'},
-    {'a', ' ', ' ', ' '},
-    {'b', ' ', ' ', ' '},
-    {'c', ' ', ' ', ' '}
-  };
+  char move [5];
+  int grid [3][3] = { 0 };
   int turn = X_TURN;
   int row = 0;
   int column = 0;
 
-  //boolean game = 0;
+  bool game = true;
+  char response = ' ';
 
-  printBoard(char grid[4][4]);
-  cout << "Enter a letter for the row, and number for the column: ";
-  cin >> move;
-  column = (int)(move[1] - '1' + 1);
-  row = (int)(move[0] - 'a' + 1);
-  cout << "row: " << row << endl;
-  cout << "column: " << column << endl;
-  if (grid[row][column] == ' ') {
-     grid[row][column] = 'X';
+  while (game == true) {
+    while (checkWin(grid, X_MOVE) == false && checkWin(grid, O_MOVE) == false
+	   && checkTie(grid) == false) {
+      cout << endl;
+      printBoard(grid);
+      cout << "Enter a letter for the row, and number for the column: ";
+      cin >> move;
+      column = (int)(move[1] - '1');
+      row = (int)(move[0] - 'a');
+      if (grid[row][column] == BLANK) {
+	if (turn == X_TURN) {
+	  grid[row][column] = X_MOVE;
+	  turn = O_TURN;
+	}
+	else {
+	  grid[row][column] = O_MOVE;
+	  turn = X_TURN;
+	}
+      }
+      else {
+	cout << "ILLEGAL  MOVE" << endl;
+      }
+    }
 
+    if (checkWin(grid, X_MOVE) == true) {
+      cout << "X wins!";
+    }
+    else if (checkWin(grid, O_MOVE) == true) {
+      cout << "O wins!";
+    }
+    else if (checkTie(grid) == true) {
+      cout << "Tie!"; 
+    }
   }
-      
-    printBoard(char grid[4][4]); 
-  
+  bool q = true;
+  while (q = true) {
+    cout << "Would you like to play again? (y or n)" << endl;
+    cin >> response;
+    if (response == 'n') {
+      q = false;
+      game = false;
+      exit(0);
+    }
+    else if (response == 'y') {
+      q = false;
+      int grid [3][3] = { 0 };
+    }
+    else {
+      cout << "Please enter y or n!" << endl;
+    }
+  }
+    
   return 0;
 }
 
-void printBoard(char grid[4][4]) {
-  for (int i = 0; i < 4; ++i) {
-    for (int j = 0; j < 4; ++j) {
-      cout << grid[i][j] << " ";
+bool checkWin(int grid[3][3], int player) {
+  if (grid[0][0] == player && grid[0][1] == player && grid[0][2] == player) {
+    return true;
+  }
+  if (grid[1][0] == player && grid[1][1] == player && grid[1][2] == player) {
+    return true;
+  }
+  if (grid[2][0] == player && grid[2][1] == player && grid[2][2] == player) {
+    return true;
+  }
+  if (grid[0][0] == player && grid[1][0] == player && grid[2][0] == player) {
+    return true;
+  }
+  if (grid[0][1] == player && grid[1][1] == player && grid[2][1] == player) {
+    return true;
+  }
+  if (grid[0][2] == player && grid[1][2] == player && grid[2][2] == player) {
+    return true;
+  }
+  if (grid[0][0] == player && grid[1][1] == player && grid[2][2] == player) {
+    return true;
+  }
+  if (grid[2][0] == player && grid[1][1] == player && grid[0][2] == player) {
+    return true;
+  }
+  return false;
+}
+
+bool checkTie(int grid[3][3]) {
+  for (int row = 0; row < 3; row++) {
+    for (int column = 0; column < 3; column++) {
+      if (grid[row][column] == BLANK) {
+	return false;
+      }
     }
-    cout << endl;
+  }
+  return true;
+}
+
+void printBoard(int grid[3][3]) {
+  cout << "\t1\t2\t3" << endl;
+  for (int row = 0; row < 3; row++) {
+    cout << (char)('a' + row) << "\t";
+    for (int column = 0; column < 3; column++) {
+      if (grid[row][column] == BLANK) {
+	cout << "\t";
+      }
+      else if (grid[row][column] == X_MOVE) {
+	cout << "X\t";
+      }
+      else if (grid[row][column] == O_MOVE) {
+	cout << "O\t";
+      }
+    }
+    cout << " " << endl; //new row
   }
   
   return;
 }
+
