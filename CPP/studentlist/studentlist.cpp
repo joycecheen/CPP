72fbf406fe37c78688ff1221c0121c89e5cbbf65
U-BSD\385533 <385533@SHS-10DEC4F83.beaverton.k12.or.us>
2019-10-23 09:09:01 -0700

    studentlist

--- CPP/studentlist/studentlist.cpp
+++ CPP/studentlist/studentlist.cpp
@@ -1,27 +1,59 @@
 #include <iostream>
 #include <stdlib.h>
 #include <vector>
-#include <stdio.h>
 #include <string.h>
 
 struct student {
-  char fname[99] = {};
-  char lname[99] = {};
+  char fname[99];
+  char lname[99];
   int id;
   float gpa;
 };
 
 using namespace std;
 
+void add(vector<student>& list, char fnameinput[99], char lnameinput[99], int idinput,
+	 float gpainput);
+void print(vector<student>& list);
+void deletestudent(vector<student>& list);
+
 int main() {
   vector<student> list;
-  
-  char fnameinput[99] = {};
-  char lnameinput[99] = {};
+  char fnameinput[99];
+  char lnameinput[99];
   int idinput;
   float gpainput;
 
-  cout <<  endl << "Enter First Name: ";
+  char response[10];
+
+  bool run = true;
+
+  cout << "To create a new entry for a student, type ADD" << endl;
+  cout << "To see all students currently stored, type PRINT" << endl;
+  cout << "To delete a student entry, type DELETE" << endl;
+  cout << "To quit, type QUIT\n" << endl;
+  
+  while (run == true) {
+    cin >> response;
+    if (strcmp(response, "ADD") == 0) {
+      add(list, fnameinput, lnameinput, idinput, gpainput);
+    }
+    else if (strcmp(response, "PRINT") == 0) {
+      print(list);
+    }
+    else if (strcmp(response, "DELETE") == 0) {
+      deletestudent(list);
+    }
+    
+  }
+  
+  
+  return 0;
+}
+
+void add (vector<student>& list, char fnameinput[99], char lnameinput[99], int idinput,
+	  float gpainput) {
+  cout << "Enter First Name: ";
   cin >> fnameinput;
   cout << "Enter Last Name: ";
   cin >> lnameinput;
@@ -29,6 +61,7 @@ int main() {
   cin >> idinput;
   cout << "Enter gpa: ";
   cin >> gpainput;
+  cout << "ADDED" << endl << endl;
 
   student s;
   strcpy(s.fname, fnameinput);
@@ -37,12 +70,36 @@ int main() {
   s.gpa = gpainput;
   list.push_back(s);
 
-  cout << gpainput;
+  return;
+}
+
+void print (vector<student>& list) {
+  for (vector<student>::iterator p = list.begin(); p != list.end(); ++p) {
+    cout << p->fname << " " << p->lname << ", ";
+    cout << p->id << ", ";
+
+    cout.precision (3);
+    cout.setf(ios::showpoint);
+    cout << p->gpa << endl;
+  }
+    cout << "PRINTED" << endl << endl;
+
+  return;
+}
+
+void deletestudent (vector<student>& list) {
+  int input;
+  
+  cout << "Please enter ID of student to delete: ";
+  cin >> input;
   
   for (vector<student>::iterator p = list.begin(); p != list.end(); ++p) {
-    cout << p->fname << " " << p->lname << endl;
-    cout << p->id << " " << p->gpa << endl;
+    if (p->id == input) {
+      p = list.erase(p);
+    }
   }
-  
-  return 0;
+
+  cout << "DELETED" << endl << endl;
+
+  return;
 }
