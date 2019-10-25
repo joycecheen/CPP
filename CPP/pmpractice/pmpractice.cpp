b13ef06de1e10d0d6886038ed79afc2459aa36c4
U-BSD\385533 <385533@SHS-10DEC4F83.beaverton.k12.or.us>
2019-10-25 09:12:10 -0700

    :(

---
+++ CPP/pmpractice/pmpractice.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+  char input[99];
+  
+  cout << "Input: ";
+  cin >> input;
+
+  for (int i = 0; i < 99; ++i) {
+    if (i = 0) {
+      
+    }
+  }
+
+    
+  return 0;
+}
