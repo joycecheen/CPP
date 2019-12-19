7b0df5ae862a89e344106312b70f931fc5b12c07
Joyce Chen <52928427+joycecheen@users.noreply.github.com>
2019-12-19 01:20:24 -0800

    o

--- linkedListsShared/Node.h
+++ linkedListsShared/Node.h
@@ -1,15 +1,17 @@
 #include "Student.h"
 
-class Node {
- public:
-  Node(Student newStudent);
-  Node* getNext();
-  Student* getStudent();
-  void setNext(Node*);
-  Node(Student*);
-  ~Node();
+// node header file
 
- private:
+class Node {
+ public: // public functions
+  Node(Student newStudent); // node new student constructor
+  Node* getNext(); // get next node pointer
+  Student* getStudent (); // get student pointer
+  void setNext(Node*); // set next pointer to corresponding node pointer
+  Node(Student*); // constructor
+  ~Node(); // destructor
+
+ private: // private variables
   Student* student;
   Node* nextNode;
 
