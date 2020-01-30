afd5cfdbd5c0a536edd32ffb95bca4ce1f8add45
U-BSD\385533 <385533@SHS-10DEC4F83.beaverton.k12.or.us>
2020-01-30 09:07:23 -0800

    h

--- CPP/shuntingyard/shuntingyard.cpp
+++ CPP/shuntingyard/shuntingyard.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
 #include <cstring>
+#include <stack>
 
 using namespace std;
 
+struct stackNode {
+  char op;
+  stackNode * next;
+};
+
+struct treeNode {
+  char op;
+  int num;
+  bool isOp;
+  treeNode * left;
+  treeNode * right;
+};
+
 int main() {
 
+  cout << "- Welcome to Shunting Yard Algorithm -" << endl << endl;
+  cout << "Use '+' for addition" << endl;
+  cout << "Use '-' for subtraction" << endl;
+  cout << "Use '*' for multiplication" << endl;
+  cout << "Use '/' for division" << endl;
+  cout << "Use '^' for power" << endl;
 
+  stackNode * head = new stackNode();
+  head -> next = NULL;
+  head -> op = ' ';
+
+  stack<treeNode*> binaryTree;
+  
+  
   return 0;
 }
