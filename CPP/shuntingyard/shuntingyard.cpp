e7eb3400777b4bb7c4076030b9b0ca14b41986bc
U-BSD\385533 <385533@SHS-10DEC4F83.beaverton.k12.or.us>
2020-02-11 09:09:09 -0800

    lol

--- CPP/shuntingyard/shuntingyard.cpp
+++ CPP/shuntingyard/shuntingyard.cpp
@@ -4,13 +4,24 @@
 
 using namespace std;
 
+/* Joyce Chen
+ * 2/11/2020
+ * Shunting-Yard Algorithm: binary expression tree and stack 
+ */
+
+// function prototypes
+void removeHead(stackNode * head);
+void addHead(stackNode * head, char newOperand);
+void prefix(treeNode * parent);
+void infix(treeNode * parent);
+
 struct stackNode {
-  char op;
+  char operand;
   stackNode * next;
 };
 
 struct treeNode {
-  char op;
+  char operand;
   int num;
   bool isOp;
   treeNode * left;
@@ -19,6 +30,7 @@ struct treeNode {
 
 int main() {
 
+  // give user instructions 
   cout << "- Welcome to Shunting Yard Algorithm -" << endl << endl;
   cout << "Use '+' for addition" << endl;
   cout << "Use '-' for subtraction" << endl;
@@ -26,23 +38,22 @@ int main() {
   cout << "Use '/' for division" << endl;
   cout << "Use '^' for power" << endl;
 
+  // create stack head
   stackNode * head = new stackNode();
   head -> next = NULL;
-  head -> op = ' ';
+  head -> operand = ' ';
 
   stack<treeNode*> binaryTree;
 
   char * infix = new char(100);
   int c = 0;
   cout << "Infix: ";
-  cin.getline(infix, 100);
+  cin.getline(infix, 100); // read in inifx notation
 
   cout << "Postfix: ";
-  for (int i = 0; i < strlen(infix); i++) {
-    if (infix[i] >= '1' && infix[i] <= '9') {
-      c += initial;
- 
-	
+  for (int i = 0; i < strlen(infix); i++)  { // run through
+    if (infix[i] >= '1' && infix[i] <= '9') { 
+      c += initial; 
     }
     else {
       if (infix[i] == ' ') { // empty space
