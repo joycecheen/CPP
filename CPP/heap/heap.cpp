0655525a165f9646a2ad9378096e034ef374b462
Joyce <385533@bsd48.org>
2020-03-05 16:36:59 +0000

    heap

--- CPP/heap/heap.cpp
+++ CPP/heap/heap.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <cstring>
 #include <fstream>
+#include <stdlib.h>
+#include <cmath>
 
 using namespace std;
+
+// i want to die ^.^
 
 void heap(int * array, int len, int i);
 
@@ -11,61 +15,133 @@ int main() {
   int num = 0;
   int * array = new int[100];
   char * arrayC = new char[500];
-  
-  cout << "Type TYPE to input by terminal line" << endl;
-  cout << "Type FILE to input by file" << endl;
-  cout << "Type QUIT to quit" << endl;
+  bool run = true;
 
-  cin >> input;
-  cin.ignore();
+  while (run == true) {
+    cout << "Type TYPE to input by terminal line" << endl;
+    cout << "Type FILE to input by file" << endl;
+    cout << "Type QUIT to quit" << endl;
 
-  if (strcmp(input, "TYPE") == 0) {
-    cout << "Enter number of elements of array: ";
-    cin >> num;
-    cout << "Enter each number followed with a space: ";
-    cin.getline(arrayC, 500);
-
-    cout << arrayC << endl;
-    
-    //parse
-    int j = 0;
-    int counter = 0;
-
-    for (int i = 0; i < 500; i++) {
-	if ((arrayC[i] != ' ') && ((arrayC[i] - '0') > 0) && ((arrayC[i] - '0') < 1000)) {
-	  array[j] = (arrayC[i] - '0');
-	  cout << "testC:" << arrayC[i] << endl;
-          cout << "testI:" << array[j] << endl;
-
-	  j++;
-	  i++;
-	}
-	else if (arrayC[i] == ' '){
-	  i++;
-	  counter++;
-	}
-    }
-  }
-  
-  /*else if (strcmp(input, "FILE") == 0) {
-    char filename[20];
-    cout << "Enter file name: ";
-    cin.get(filename, 20);
+    cin >> input;
     cin.ignore();
 
-                                 
-    ifstream file(filename);
-    if (file.is_open) {
-      while (getline (file, ))
+    if (strcmp(input, "TYPE") == 0) {
+      cout << "Enter number of elements of array: ";
+      cin >> num;
+      cin.ignore();
+      cout << "Enter each number followed with a space: ";
+      cin.getline(arrayC, 500);
+
+      cout << arrayC << endl;
+
+      //parse
+      int count;
+      int exp;
+      int num;
+      int total;
+      int j = 0;
+
+      for (int i = 0; i < 100; i++) {
+	//Null character
+	if (arrayC[i] == '\0') {
+	  i = 100;
+	}
+	//Single digit number
+	else if (isdigit(arrayC[i]) && !isdigit(arrayC[i+1]) && arrayC[i] != '\0') {
+	  array[i] = (int)arrayC[i] - 48;
+	}
+	//Multiple digit number
+	else if (isdigit(arrayC[i]) && isdigit(arrayC[i+1])) {
+	  //While input[i] isn't a space
+	  while (isdigit(arrayC[i])) {
+	    count++;
+	    i++;
+	  }
+	  //If input[i] is a space
+	  if (!isdigit(arrayC[i])) {
+	    for (int j = count; j > 0; j--) {
+	      num = input[i-j] - 48;
+	      exp = pow(10, j-1);
+	      total += num * exp;
+	    }
+	  }
+	  array[i] = total;
+	  total = count = 0;
+	}
+      }
+      int * heaparr = new int[100];
+      heaparr[0] = 0;
+      //Place parsed values into another array with no zeros
+      for (int i = 0; i < 100; i++) {
+	if (array[i] != 0) {
+	  heaparr[j] = array[i];
+	  j++;
+	  count++;
+	}
+      }
+
+      /*
+      //Construct the max heap
+      buildHeap(heaparr, count);
+      //Display heap
+      displayVisual(heaparr, count);
+      printHeap(heaparr, outputarr, count, o, h);
+      */
+      /*
+      //parse
+      int j = 0;
+      int counter = 0;
+
+      for (int i = 0; i < 500; i++) {
+      if ((arrayC[i] != ' ') && ((arrayC[i] - '0') > 0) && ((arrayC[i] - '0') < 1000)) {
+      array[j] = (arrayC[i] - '0');
+      cout << "testC:" << arrayC[i] << endl;
+      cout << "testI:" << array[j] << endl;
+
+      j++;
+      i++;
+      }
+      else if (arrayC[i] == ' '){
+      i++;
+      counter++;
+      }
+      }
+      cout << counter;*/
     }
-  }*/
   
-  else if (strcmp(input , "QUIT") == 0) {
-    return 0;
+    /*else if (strcmp(input, "FILE") == 0) {
+      char filename[20];
+      cout << "Enter file name: ";
+      cin.get(filename, 20);
+      cin.ignore();
+
+                                 
+      ifstream file(filename);
+      if (file.is_open) {
+      while (getline (file, ))
+      }
+      }*/
+  
+    else if (strcmp(input , "QUIT") == 0) {
+      return 0;
+    }
   }
 
   return 0;
 }
+/*
+void print(int * array, int depth) {
+  if (array != NULL) {
+    print(array -> right, depth++)
+  }
+  for(i < depth) {
+    cout << '\t';
+  }
+  cout << array -> value;
+  if (left != NULL) {
+    print(array -> left, depth++);
+  }
+  }*/
 
 void heap(int * array, int len, int i) {
   int max = i;
