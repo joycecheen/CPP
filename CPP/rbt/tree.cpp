271628d84101952e96fc5b6a5d9bcbd10fe973d6
Joyce <385533@bsd48.org>
2020-04-14 10:12:41 +0100

    insert

--- CPP/rbt/tree.cpp
+++ CPP/rbt/tree.cpp
@@ -8,7 +8,29 @@ tree::tree() {
 }
 
 void tree::insert(int value, node * root) {
-
+  if (root->getValue() == 0) {
+    root->setValue(value);
+  }
+  else if (value <= root->getValue()) {
+    if (root->getLeft() == NULL) {
+      node* temp = new node(value, 1, root, NULL, NULL);
+      root->setLeft(temp);
+      fix(root->getLeft());
+    }
+    else {
+      insert(value, root->getLeft());
+    }
+  }
+  else {
+    if (root->getRight() == NULL) {
+      node* temp = new node(value, 1, root, NULL, NULL);
+      root->setRight(temp);
+      fix(root->getRight());
+    }
+    else {
+      insert(value, root->getRight());
+    }
+  }
 }
 
 void tree::print(int spaces, node * root) {
